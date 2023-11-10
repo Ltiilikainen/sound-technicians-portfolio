0a8121e792a33c57e5b98780806f519d08fb7eca
Laura Tiilikainen <laura.tiilikainen@hotmail.com>
2023-11-10 12:18:26 +0200

    fixed styling and css classes

--- src/client/components/HomePage.tsx
+++ src/client/components/HomePage.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './HomePage.css';
 
 const HomePage = () => {
     return (
         <>
             <div className="hero-img">
-                <img src='./src/files/hero-img.png'></img>
+                <span className="hero-text">Firstname &#34;Nickname&#34; Lastname</span>
             </div>
             <div className="container">
                 <h1>Home Page</h1>
